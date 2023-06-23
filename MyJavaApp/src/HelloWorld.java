d22d0d4ecebc621dae6257167afc417476512c53
Majrul Ansari <majrul@gmail.com>
2023-06-23 19:16:45 +0530

    some changes again

--- MyJavaApp/src/HelloWorld.java
+++ MyJavaApp/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Learning Git Integration!");
+		System.out.println("Saving work on Github!");
 	}
 }
